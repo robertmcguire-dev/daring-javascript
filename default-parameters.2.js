45389418fdd579c477b30be3e150586fe19c472f
robertmcguire-dev <52634059+robertmcguire-dev@users.noreply.github.com>
2019-11-17 14:49:18 +0000

    Update default-parameters.2.js

--- default-parameters.2.js
+++ default-parameters.2.js
@@ -1,13 +1,11 @@
 // default-parameters.2.js
 function t (fogLevel = 1, sparkLevel = fogLevel) {
   console.log(`Fog Level: ${fogLevel} and spark_level: ${sparkLevel}`)
-  // Fog Level: 10 and spark_level: 10
 }
 
 function s (fogLevel = 10, sparkLevel = fogLevel * 10) {
   console.log(`Fog Level: ${fogLevel} and spark_level:
        ${sparkLevel}`)
-  // Fog Level: 10 and spark_level: 100
 }
-t(10)
-s(10)
+t(10) // Fog Level: 10 and spark_level: 10
+s(10) // Fog Level: 10 and spark_level: 100
